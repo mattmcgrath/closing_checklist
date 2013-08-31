e271039136e48bacc41bfe9e8d016da75367ecb0
Matthew R. McGrath <matthew.r.mcgrath@gmail.com>
2013-08-30 21:26:48 -0400

    Setup basic Ryan Bates javascript remove behavior.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,6 @@
 class Deal < ActiveRecord::Base
   attr_accessible :name, :phases_attributes
   has_many :phases
-  accepts_nested_attributes_for :phases
+  accepts_nested_attributes_for :phases, allow_destroy: true
   validates :name, presence: true, uniqueness: true
 end
